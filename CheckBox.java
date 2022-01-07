72cbfb27368617d78d3631b3b443614e2b5b3478
Abiramiramadoss <96653010+Abiramiramadoss@users.noreply.github.com>
2022-01-07 20:33:51 +0530

    Add files via upload

---
+++ CheckBox.java
@@ -0,0 +1,32 @@
+package week2.day2.assignment;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CheckBox {
+
+	public static void main(String[] args) {
+		 WebDriverManager.chromedriver().setup();
+		 ChromeDriver driver = new ChromeDriver();
+		 driver.get("http://www.leafground.com/pages/checkbox.html");
+		 driver.manage().window().maximize();
+		 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
+		 driver.findElement(By.xpath("//div[text()='Java']/input")).click();
+		 driver.findElement(By.xpath("//div[text()='C']/input")).click();
+		 driver.findElement(By.xpath("//div[text()='C++']/input")).click();
+		 driver.findElement(By.xpath("//div[text()='I am Selected']/input")).click();
+		 driver.findElement(By.xpath("//div[text()='Option 1']/input")).click();
+		 driver.findElement(By.xpath("//div[text()='Option 2']/input")).click();
+		 driver.findElement(By.xpath("//div[text()='Option 3']/input")).click();
+		 driver.findElement(By.xpath("//div[text()='Option 4']/input")).click();
+		 driver.findElement(By.xpath("//div[text()='Option 5']/input")).click();
+		 
+		 
+
+	}
+
+}
